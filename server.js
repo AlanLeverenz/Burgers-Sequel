37fec00b6ca0447544f3ad7d915a09bbe27420ba
Alan Leverenz <awleverenz@aol.com>
2019-11-18 00:03:22 -0500

    added 'equal' handlebars helper

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 require("dotenv").config();
-var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var exphbs = require('express-handlebars');
+var path = require('path');
 
-// SET EXPRESS TO APP VAR
+// INSERT ------
+
+var express = require('express');
+var exphbs = require('express-handlebars');
 var app = express();
 
-// DEFINE PORT
-var PORT = process.env.PORT || 8080;
+//Here you can pass helpers that you would normally define in registerHelpers
+//and you can also define stuff like `defaultLayout` and `partialsDir`
+var hbs = exphbs.create({
+    helpers: {
+      equal : function(lvalue, rvalue, options) {
+        if (arguments.length < 3)
+            throw new Error("Handlebars Helper equal needs 2 parameters");
+        if ( lvalue!=rvalue ) {
+            return options.inverse(this);
+        } else {
+            return options.fn(this);
+        }
+      }
+    },
+    defaultLayout: 'main',
+    partialsDir: ['views/partials/']
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+// END INSERT ------
+
+// HANDLEBARS VIEW ENGINE
+// app.engine('handlebars', exphbs({
+//   defaultLayout: 'main'
+// }));
 
 // SET PUBLIC AS ROOT FOLDER FOR EXPRESS
 app.use(express.static(__dirname + '/public'));
@@ -20,11 +47,48 @@ app.use(bodyParser.urlencoded({
 
 app.use(methodOverride('_method'));
 
-// HANDLEBARS VIEW ENGINE
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+
+// COMPARE HELPER
+// exphbs.registerHelper(‘compare’, function (lvalue, operator, rvalue, options) {
+
+//   var operators, result;
+    
+//   if (arguments.length < 3) {
+//       throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+//   }
+  
+//   if (options === undefined) {
+//       options = rvalue;
+//       rvalue = operator;
+//       operator = "===";
+//   }
+  
+//   operators = {
+//       '==': function (l, r) { return l == r; },
+//       '===': function (l, r) { return l === r; },
+//       '!=': function (l, r) { return l != r; },
+//       '!==': function (l, r) { return l !== r; },
+//       '<': function (l, r) { return l < r; },
+//       '>': function (l, r) { return l > r; },
+//       '<=': function (l, r) { return l <= r; },
+//       '>=': function (l, r) { return l >= r; },
+//       'typeof': function (l, r) { return typeof l == r; }
+//   };
+  
+//   if (!operators[operator]) {
+//       throw new Error("Handlerbars Helper 'compare' doesn't know the operator " + operator);
+//   }
+  
+//   result = operators[operator](lvalue, rvalue);
+  
+//   if (result) {
+//       return options.fn(this);
+//   } else {
+//       return options.inverse(this);
+//   }
+
+// });
+
 
 // MODELS
 var db = require("./models");
@@ -34,6 +98,9 @@ var syncOptions = { force: false };
 if (process.env.NODE_ENV === "test") {
     syncOptions.force = true;
   }
+
+  // DEFINE PORT
+var PORT = process.env.PORT || 8080;
 
   // STARTING THE SERVER, SYNC THE MODELS
 db.sequelize.sync(syncOptions).then(function() {
